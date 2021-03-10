4beaa4f67fbe9592ea5a8aedde8bb0621f218af5
Vincenzo Mezzio <48773747+CenzOh@users.noreply.github.com>
2021-03-09 21:45:05 -0500

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,13 @@
+<!doctype html>
+
+<style>
+.footer{ padding: 10px; text-align: center; font-family: Helvetica; }
+</style>
+
+<body></body>
+
+    <div class="footer">
+          <p>© Vincenzo Mezzio 2021</p>
+    </div>
+
+</html>
