184f19c9560ce9b5502a3cd171d1a5086062c0c1
Vincenzo Mezzio <48773747+CenzOh@users.noreply.github.com>
2021-03-09 21:49:55 -0500

    Update portfolioFirstDraft.php

--- portfolioFirstDraft.php
+++ portfolioFirstDraft.php
@@ -49,44 +49,7 @@
       <body>
 
 <!-- Nav bar -->
-        <nav class="navbar sticky-top navbar-expand-lg navbar-dark bg-dark"> <!-- Dark theme -->
-          <a class="navbar-brand" href="/../vincenzoMezzioPortfolio.php">Vincenzo Mezzio</a> <!-- Left most side of navbar -->
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
-              <li class="nav-item active">
-                <a class="nav-link" href="/../vincenzoMezzioPortfolio.php">Home <span class="sr-only">(current)</span></a>
-              </li>
-              <!-- The current ^^ keeps "Home" highlighted-->
-              <li class="nav-item">
-                <a class="nav-link" href="https://github.com/CenzOh" target="_blank">GitHub</a> <!-- Target=_blank opens the link in a new tab -->
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="https://www.linkedin.com/in/vincenzomezzio/" target="_blank">LinkedIn</a> 
-              </li>
-              <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink-4" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                  Assignments
-                </a>
-                <div class="dropdown-menu dropdown-info" aria-labelledby="navbarDropdownMenuLink-4">
-                  <a class="dropdown-item" href="/../CSC226Assignment2/partB.php">Assignment 2 Part B</a>
-                  <a class="dropdown-item" href="/../CSC226Assignment2/partC.php">Assignment 2 Part C</a>
-                  <a class="dropdown-item" href="/../CSC226Assignment2/partD.php">Assignment 2 Part D</a>
-                  <div class="dropdown-divider"></div>
-                  <a class="dropdown-item" href="#">Coming Soon!</a>
-                </div>
-              </li>
-            </ul>
-          </div>
-        </nav>
-        <!-- Adding the script allowed the dropdown menu to work -->
-        <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-
+        <?php include "navbar.php"; ?> 
 <!-- header -->
         <div class="fullCard" style="font-family: Garamond">
         <div style="text-align: center">
@@ -163,9 +126,7 @@
           </div>
         </body>
         <!-- footer -->
-        <div class="footer">
-          <p>© Vincenzo Mezzio 2021</p>
-        </div>
+        <?php include "footer.php"; ?>
 </html> 
 
 
